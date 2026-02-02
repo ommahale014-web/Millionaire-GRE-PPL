ff56a72b73943cdb9282365b97d95b90d61b3cf7
Bhavesh-Gadekar <bhaveshgadekar.rmdstic.comp@gmail.com>
2026-02-02 15:41:02 +0530

    Renamed sidebar testAttempts to Performace Analytics

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -54,7 +54,7 @@ export default function Sidebar() {
     },
 
     {
-      name: "Test Attempts",
+      name: "Performance Analytics",
       icon: ListChecks,
       href: "/admin/testAttempts",
       match: "/admin/testAttempts",
